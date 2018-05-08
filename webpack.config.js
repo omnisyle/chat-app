471bfa2522a6727eb39e3bf65b8c48c0255b2972
Si Le <omnisyle@gmail.com>
2018-05-08 10:22:39 -0400

    fixes config to not hash css class names

--- webpack.config.js
+++ webpack.config.js
@@ -44,9 +44,8 @@ module.exports = {
           {
             loader: "css-loader",
             options: {
-              modules: true,
+              modules: false,
               importLoaders: 1,
-              localIdentName: "[name]_[local]_[hash:base64]",
               sourceMap: true,
               minimize: true
             }
