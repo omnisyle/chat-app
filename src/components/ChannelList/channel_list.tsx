f7bea7988c080771a0b3b05fc3b790493bb225e3
Si Le <omnisyle@gmail.com>
2018-05-09 17:50:41 -0400

    adds ChannelList component

---
+++ src/components/ChannelList/channel_list.tsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+
+const ChannelList = () => (
+  <div className="channels-list">
+    <div className="channel">
+      <div className="profile-picture-container">
+        <img
+          className="profile-picture"
+          src="https://api.adorable.io/avatars/60/abott@adorable.png"
+          alt="adorable-avatar"
+        />
+      </div>
+      <div className="channel-info">
+        <h4 className="channel-title">
+          Username
+            </h4>
+      </div>
+    </div>
+    <div className="channel active">
+      <div className="profile-picture-container">
+        <img
+          className="profile-picture"
+          src="https://api.adorable.io/avatars/60/abott@adorable.png"
+          alt="adorable-avatar"
+        />
+      </div>
+      <div className="channel-info">
+        <h4 className="channel-title">
+          Username
+            </h4>
+      </div>
+    </div>
+  </div>
+);
+
+export default ChannelList;
---
+++ src/components/ChannelList/index.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import ChannelList from "./channel_list";
+import "./styles.scss";
+
+class ChannelListContainer extends React.Component {
+
+  render() {
+    return (
+      <ChannelList />
+    );
+  }
+}
+
+export default ChannelListContainer;
