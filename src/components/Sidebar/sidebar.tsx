47a58a688a536a48e7b06d27eee2da08e6bea831
Si Le <omnisyle@gmail.com>
2018-05-09 17:50:58 -0400

    render ChannelList in Sidebar component

--- src/components/Sidebar/sidebar.tsx
+++ src/components/Sidebar/sidebar.tsx
@@ -1,14 +1,15 @@
 import * as React from "react";
+import ChannelList from "../ChannelList";
 
 const Sidebar = () => (
   <div
     className="sidebar"
   >
     <div className="sidebar-header">
-      <span>Channels</span>
+      <h2 className="sidebar-title">Channels</h2>
     </div>
     <div className="sidebar-body">
-
+      <ChannelList />
     </div>
   </div>
 );
