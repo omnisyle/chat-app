4c259fad4600aadb16f05e859529d633b3c37202
Si Le <omnisyle@gmail.com>
2018-07-04 01:27:00 -0400

    adds PrivateRoute component for authenticated routes

---
+++ src/components/PrivateRoute/index.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Route, RouteComponentProps, Redirect, RouteProps } from "react-router-dom";
+import { UserConsumer } from "../../services/user_context";
+import { isEmpty } from "../../utils/";
+
+const PrivateRoute = ({ component: Component, ...rest } : RouteProps) => {
+
+   const renderComponent = (props: RouteComponentProps<any, any>) : React.ReactNode => {
+    return (
+      <UserConsumer>
+        {
+          (store) => {
+            if (isEmpty(store.state.user)) {
+              return (<Redirect
+                to={{
+                    pathname: "/login",
+                    state: { from: rest.location }
+                  }}
+              />);
+            } else {
+              return (<Component {...props} />);
+            }
+          }
+        }
+      </UserConsumer>
+    );
+  };
+
+  return (
+    <Route
+      {...rest}
+      render={renderComponent}
+    />
+  );
+};
+
+export default PrivateRoute;
